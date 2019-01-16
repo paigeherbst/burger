86f3ba6b1abd9ba64ac8808bbdb4d2435c2ac5b2
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-01-16 16:14:48 -0700

    changes

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var connection;
+
+// Setting up connection for use on Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+// Localhost connection
+else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Database!DreamGirl28',
+    database : 'burgers_db'
+  });
+};
+
+module.exports = connection;
